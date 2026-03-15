de44e435d358e372a1256013d7b188676032278f
chaewonkong <chaewonkong@gmail.com>
2026-03-15 14:22:36 +0900

    feat: print feed title and entries' titles

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let body = response.text().await?;
     println!("가져온 바이트 수: {}", body.len());
 
+    let feed = feed_rs::parser::parse(body.as_bytes())?;
+
+    if let Some(title) = &feed.title {
+        println!("피드 제목: {}", title.content);
+    }
+
+    println!("아이템 수: {}", feed.entries.len());
+
+    for entry in &feed.entries {
+        if let Some(title) = &entry.title {
+            println!("- {}", title.content)
+        }
+    }
+
     Ok(())
 }
